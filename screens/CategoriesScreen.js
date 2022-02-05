03d82c2cbadf35956b17b01bab1d1c7670be083d
Avnish <avnishchhabra5@gmail.com>
2022-02-05 15:34:00 +0530

    dummy data with category model added

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,18 +1,32 @@
-import { StyleSheet, Text, View, TouchableWithoutFeedback } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableWithoutFeedback,
+  FlatList,
+} from "react-native";
 import React from "react";
+import { CATEGORIES } from "../data/dummy-data";
+
+const gridItem = (itemData) => {
+  return (
+    <View style={styles.gridItem}>
+      <Text>{itemData.item.title}</Text>
+    </View>
+  );
+};
 
 const CategoriesScreen = ({ navigation }) => {
-  return (
-    <TouchableWithoutFeedback
-      onPress={() => navigation.navigate("category_meals")}
-    >
-      <View>
-        <Text>CategoriesScreen</Text>
-      </View>
-    </TouchableWithoutFeedback>
-  );
+  return <FlatList numColumns={2} data={CATEGORIES} renderItem={gridItem} />;
 };
 
 export default CategoriesScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  gridItem: {
+    backgroundColor: "red",
+    flex: 1,
+    margin: 15,
+    height: 150,
+  },
+});
