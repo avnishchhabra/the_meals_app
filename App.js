33288bd9b2d9b9d05f5b1034bbace539ce30095a
Avnish <avnishchhabra5@gmail.com>
2022-02-05 13:08:30 +0530

    basic project and navigation setup

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as FONT from "expo-font";
+import AppLoading from "expo-app-loading";
+import { useState } from "react";
+import CategoriesScreen from "./screens/CategoriesScreen";
+import CategoryMealsScreen from "./screens/CategoryMealsScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+const Stack = createNativeStackNavigator();
+const fetchFonts = () => {
+  return FONT.loadAsync({
+    sansBold: require("./assets/fonts/sans_bold.ttf"),
+  });
+};
 export default function App() {
+  const [appLoaded, setAppLoaded] = useState(false);
+  if (!appLoaded) {
+    return (
+      <AppLoading
+        onError={() => console.log("error")}
+        startAsync={fetchFonts}
+        onFinish={() => setAppLoaded(true)}
+      />
+    );
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="categories" component={CategoriesScreen} />
+        <Stack.Screen name="category_meals" component={CategoryMealsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
