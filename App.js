e34e24d73355dd95d92fbadf9cc54a33b06212d0
Avnish <avnishchhabra5@gmail.com>
2022-02-05 21:41:52 +0530

    tabs added at bottom

--- App.js
+++ App.js
@@ -6,11 +6,16 @@ import { useState } from "react";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import CategoryMealsScreen from "./screens/CategoryMealsScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
+import FavouritesScreen from "./screens/FavouritesScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import CustomHeader from "./components/CustomHeader";
 import colors from "./constants/colors";
 import { enableScreens } from "react-native-screens";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+
+const Tab = createBottomTabNavigator();
 
 enableScreens();
 
@@ -31,28 +36,53 @@ export default function App() {
       />
     );
   }
+  const CategoryTab = () => (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: colors.primary,
+        },
+        headerTintColor: "white",
+        // headerTitle: () => <CustomHeader />,
+      }}
+    >
+      <Stack.Screen name="categories" component={CategoriesScreen} />
+      <Stack.Screen name="category_meals" component={CategoryMealsScreen} />
+      <Stack.Screen
+        options={({ route }) => ({
+          title: route.params.title,
+          headerRight: () => <Text>hii</Text>,
+        })}
+        name="meal_details"
+        component={MealDetailScreen}
+      />
+    </Stack.Navigator>
+  );
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: colors.primary,
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Catagories") {
+              iconName = focused
+                ? "ios-information-circle"
+                : "ios-information-circle-outline";
+            } else if (route.name === "Favourites") {
+              iconName = focused ? "ios-list-circle" : "ios-list";
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
           },
-          headerTintColor: "white",
-          // headerTitle: () => <CustomHeader />,
-        }}
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "gray",
+        })}
       >
-        <Stack.Screen name="categories" component={CategoriesScreen} />
-        <Stack.Screen name="category_meals" component={CategoryMealsScreen} />
-        <Stack.Screen
-          options={({ route }) => ({
-            title: route.params.title,
-            headerRight: () => <Text>hii</Text>,
-          })}
-          name="meal_details"
-          component={MealDetailScreen}
-        />
-      </Stack.Navigator>
+        <Tab.Screen name="Catagories" component={CategoryTab} />
+        <Tab.Screen name="Favourites" component={FavouritesScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
